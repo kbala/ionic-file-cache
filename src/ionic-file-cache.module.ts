9ea60b69ae89cde9db05204884977475b9c569ee
Balasubramanian Kasamuthu <kbala@Balasubramanians-MacBook-Pro.local>
2019-01-24 19:33:26 +0530

    0.0.5

--- src/ionic-file-cache.module.ts
+++ src/ionic-file-cache.module.ts
@@ -1,15 +1,15 @@
-import { ModuleWithProviders, NgModule } from "@angular/core";
-import { File } from "@ionic-native/file";
-import { FileTransfer } from "@ionic-native/file-transfer";
-import { IonicFileCacheService } from "./ionic-file-cache.service";
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { File } from '@ionic-native/file';
+import { FileTransfer } from '@ionic-native/file-transfer';
+import { IonicFileCacheService } from './ionic-file-cache.service';
 @NgModule({
-    imports: [IonicFileCacheModule.forRoot()]
+  imports: [IonicFileCacheModule.forRoot()],
 })
 export class IonicFileCacheModule {
-    public static forRoot(): ModuleWithProviders {
-        return {
-            ngModule: IonicFileCacheModule,
-            providers: [IonicFileCacheService, File, FileTransfer]
-        }
-    }
+  public static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: IonicFileCacheModule,
+      providers: [IonicFileCacheService, File, FileTransfer],
+    };
+  }
 }
